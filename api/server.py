66ead46da0e064eb9111ca8706e89ba4656152cc
Michał Gniadek <mgniadek@interia.pl>
2022-12-11 22:00:41 +0100

    Redirect

--- api/server.py
+++ api/server.py
@@ -1,10 +1,23 @@
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import html
+from sanic.request import Request
+from sanic.websocket import WebSocketConnection
 
 app = Sanic("MyHelloWorldApp")
+# with open("choice.html", encoding='utf-8') as f:
+#     app.choice_html = f.read()
+# print(app.choice_html)
 
 
-@app.route('/')
-@app.route('/<path:path>')
-async def index(request, path=""):
-    return text(path)
+# @app.get("/api/server")
+# async def index(_: Request):
+#     return html(app.choice_html)
+
+
+@app.websocket("api/server/connect")
+async def connect(_: Request, ws: WebSocketConnection):
+    async for msg in ws:
+        await ws.send(msg)
+
+if __name__ == "__main__":
+    app.run()
