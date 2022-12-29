8cf9f373f1b7f035d617c4ad914ecdbddd23286f
Michał Gniadek <mgniadek@interia.pl>
2022-12-29 21:59:07 +0100

    Server mostly working

--- AHP_algo.py
+++ AHP_algo.py
@@ -1,13 +1,41 @@
 from enum import Enum
+from random import shuffle
 
 import numpy as np
 
 
+def img(s: str): return f'<img src="{s}" alt="{s.split(".")[0]}">'
+def txt(s: str): return f'<div>{s}</div>'
+
+
+def shuffled_range(*args):
+    l = list(range(*args))
+    print(l)
+    shuffle(l)
+    print(l)
+    return l
+
+
 class Category(Enum):
-    PRICE = "price",
+    PRICE = "price"
     LOCATION = "location"
     REVIEWS = "reviews"
-    FOOD_TYPE = "type"
+    FOOD_TYPE = "food_type"
+
+    def pretty(self):
+        if self == Category.PRICE:
+            return txt("💰 Price")
+        if self == Category.LOCATION:
+            return txt("🏃 Location")
+        if self == Category.REVIEWS:
+            return txt("⭐ Reviews")
+        if self == Category.FOOD_TYPE:
+            return txt("🍽️ Food Type")
+
+    def pretty_restaurant(self, value):
+        if self == Category.PRICE:
+            return img(getattr(value, self.value))
+        return txt(getattr(value, self.value))
 
 
 class Restaurant:
@@ -20,32 +48,33 @@ class Restaurant:
 
 
 class AHP:
-    restaurants = []
+    restaurants = [Restaurant("McDonalds", "mcdonalds.jpg", 1, 1, 1),
+                   Restaurant("Poco Loco", "poco-loco.png", 2, 2, 2)]
     categories = [c for c in Category]
 
 
 class Expert:
     def __init__(self, ahp: AHP):
+        self.ahp = ahp
         self.cat_num = len(ahp.categories)
         self.res_num = len(ahp.restaurants)
         self.categories = ahp.categories
         self.restaurants = ahp.restaurants
-        self.c_cat = {c: np.zeros((self.res_num, self.res_num), int)
+        self.c_cat = {c: np.zeros((self.res_num, self.res_num), float)
                       for c in ahp.categories}
-        self.c_prio = np.zeros((self.cat_num, self.cat_num), int)
+        self.c_prio = np.zeros((self.cat_num, self.cat_num), float)
         for c in ahp.categories:
             for i in range(self.res_num):
                 self.c_cat[c][i][i] = 1
         for i in range(self.cat_num):
             self.c_prio[i][i] = 1
 
-    def get_next_cat_request(self, cat: Category):
-        for i in range(self.res_num):
-            for j in range(i+1, self.res_num):
-                if self.c_cat[cat][i][j] == 0:
-                    return cat, (self.restaurants[i], self.restaurants[j])
-
-        return cat, (None, None)
+    def get_next_cat_request(self):
+        for cat in self.ahp.categories:
+            for i in shuffled_range(self.res_num):
+                for j in shuffled_range(i+1, self.res_num):
+                    if self.c_cat[cat][i][j] == 0:
+                        return cat, self.restaurants[i], self.restaurants[j]
 
     def set_cat_answer(self, cat: Category, res1: Restaurant, res2: Restaurant, result):
         i = self.restaurants.index(res1)
@@ -54,8 +83,8 @@ class Expert:
         self.c_cat[cat][j][i] = 1 / result
 
     def get_next_prio_request(self):
-        for i in range(self.cat_num):
-            for j in range(i+1, self.cat_num):
+        for i in shuffled_range(self.cat_num):
+            for j in shuffled_range(i+1, self.cat_num):
                 if self.c_prio[i][j] == 0:
                     return self.categories[i], self.categories[j]
 
@@ -64,3 +93,6 @@ class Expert:
         j = self.categories.index(cat2)
         self.c_prio[i][j] = result
         self.c_prio[j][i] = 1 / result
+
+    def is_finished(self):
+        return self.get_next_cat_request() is None and self.get_next_prio_request() is None
--- server.py
+++ server.py
@@ -1,34 +1,122 @@
+import array
+from dataclasses import dataclass, field
+import random
+import string
+from sys import argv
 from sanic import Sanic
 from sanic.request import Request
 from inspect import isawaitable
 from functools import wraps
 from sanic_ext import render
 
+from AHP_algo import AHP, Expert
+
 app = Sanic("MIAPD")
 app.static("/", "./static")
+
+
+@dataclass
+class User:
+    expert: Expert
+    curr_choice: any = None
+
+
+@dataclass
+class Room:
+    ahp: AHP = AHP()
+    users: dict[str, User] = field(default_factory=dict)
+
+
+class RoomManager:
+    def __init__(self) -> None:
+        self.rooms: dict[str, Room] = {}
+
+    def random_key() -> str:
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+
+    def new_room(self) -> str:
+        key = RoomManager.random_key()
+
+        while key in self.rooms:
+            key = RoomManager.random_key()
+
+        self.rooms[key] = Room()
+
+        return key
+
+    def add_user(self, code: str, name: str):
+        if code in self.rooms:
+            ahp = self.rooms[code].ahp
+            if name not in self.rooms[code].users:
+                self.rooms[code].users[name] = User(Expert(ahp))
+                return True
+        return False
+
+    def get_next_choice(self, code: str, name: str):
+        user = self.rooms[code].users[name]
+
+        def try_get_prio():
+            prio_choice = user.expert.get_next_prio_request()
+            if prio_choice:
+                user.curr_choice = "prio", prio_choice
+                return prio_choice[0].pretty(), prio_choice[1].pretty()
+
+        def try_get_cat():
+            cat_choice = user.expert.get_next_cat_request()
+            if cat_choice:
+                user.curr_choice = "cat", cat_choice
+                return cat_choice[0].pretty_restaurant(cat_choice[1]), cat_choice[0].pretty_restaurant(cat_choice[2])
+
+        if random.choice([True, False]):
+            return try_get_prio() or try_get_cat()
+        else:
+            return try_get_cat() or try_get_prio()
+
+    def set_next_choice(self, code: str, name: str, value):
+        user = self.rooms[code].users[name]
+
+        choice_type, choice_args = user.curr_choice
+        if choice_type == "prio":
+            user.expert.set_prio_answer(*choice_args, value)
+        elif choice_type == "cat":
+            user.expert.set_cat_answer(*choice_args, value)
+        else:
+            raise ValueError(f"Incorrect choice_type {choice_type}")
+
+    def get_result(self, code: str):
+        room = self.rooms[code]
+
+        for user in room.users.values():
+            if not user.expert.is_finished():
+                break
+        else:
+            return "McDonalds"
+
+
+rooms = RoomManager()
 
 
 def template_with_cookies(template_path: str):
     def inner(f):
         @wraps(f)
-        @app.ext.template(template_path)
         async def decorated_function(request, *args, **kwargs):
             response = f(request, *args, **kwargs)
 
             if isawaitable(response):
                 response = await response
 
-            if type(response) == tuple:
+            template = template_path
+            if template:
                 context, cookies = response
             else:
-                context, cookies = response, {}
+                template, context, cookies = response
 
-            response = await render(context=context)
+            response = await render(template, context=context)
 
             for k, v in cookies.items():
                 if v:
                     response.cookies[k] = v
-                    response.cookies[k]["samesite"] = "Strict"
+                    response.cookies[k]["samesite"] = "strict"
                 else:
                     del response.cookies[k]
 
@@ -42,22 +130,66 @@ def template_with_cookies(template_path: str):
 @app.get("/")
 @template_with_cookies("index.html")
 async def index(_: Request):
-    return {}, {'name': None, 'code': None}
+    return {'notice': 'Leave code empty for a new room'}, {'name': None, 'code': None}
 
 
 @app.post("login")
-@template_with_cookies("chooser.html")
+@template_with_cookies(None)
 async def login(rq: Request):
     name, code = rq.form.get('name'), rq.form.get('code')
-    return {'option_a': 'asdasdasd asdadasdasd asd asd asd as dasd as dasdsasda', 'option_b': 'b'}, {'name': name, 'code': code}
+
+    if not code:
+        code = rooms.new_room()
+    else:
+        code = code.upper()
+
+    if rooms.add_user(code, name):
+        a, b = rooms.get_next_choice(code, name)
+
+        return (
+            "chooser.html",
+            {
+                'room_code': code,
+                'option_a': a,
+                'option_b': b
+            },
+            {'name': name, 'code': code}
+        )
+    else:
+        return "login.html", {'notice': 'Invalid room code!'}, {}
 
 
 @app.post("do")
-@template_with_cookies("chooser.html")
+@template_with_cookies(None)
 async def do_stuff(rq: Request):
     name, code = rq.cookies['name'], rq.cookies['code']
-    value = rq.form.get("value")
-    return {'option_a': 'a', 'option_b': 'b'}
+    value = float(rq.form.get("value"))
+
+    rooms.set_next_choice(code, name, value)
+
+    choice = rooms.get_next_choice(code, name)
+
+    if choice:
+        return "chooser.html", {'room_code': code, 'option_a': choice[0], 'option_b': choice[1]}, {}
+    else:
+        result = rooms.get_result(code)
+        if result:
+            return "results.html", {"result": result}, {}
+        return "waiting.html", {}, {}
+
+
+@app.post("results")
+@template_with_cookies(None)
+async def results(rq: Request):
+    _, code = rq.cookies['name'], rq.cookies['code']
+
+    result = rooms.get_result(code)
+    if result:
+        return "results.html", {"result": result}, {}
+    return "waiting.html", {}, {}
 
 if __name__ == "__main__":
-    app.run(dev=True)
+    dev = True
+    if len(argv) == 2 and argv[1] == "prod":
+        dev = False
+    app.run(dev=dev)
